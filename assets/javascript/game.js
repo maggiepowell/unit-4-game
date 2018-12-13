147c8001b079519a3ab52729e867325cefd46f01
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2018-12-12 16:13:22 -0800

    added to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
-var computerNumber = Math.floor((Math.random() * 120) + 19);
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+var startGame = $("#startButton").on("click", function() {
+    var computerNumber = Math.floor((Math.random() * 120) + 19);
+    console.log("test");
+})
+
+var addValueToCrystal //function to be passed to each crystal
+var sapphire
+var emerald
+var diamond
+var ruby
