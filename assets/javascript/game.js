9962ba31cc335acae53273ccff3451a3d21470f2
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2018-12-12 16:51:45 -0800

    added more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,30 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
+var addValueToCrystals 
+var computerNumber = 0
+var sapphire = 0
+var emerald = 0
+var diamond = 0
+var ruby = 0
+
 var startGame = $("#startButton").on("click", function() {
-    var computerNumber = Math.floor((Math.random() * 120) + 19);
+    computerNumber = Math.floor((Math.random() * 120) + 19);
+    $(".computerNumber").text(computerNumber + " is your number to match!");
+
+    sapphire = Math.floor((Math.random() * 12) + 1);
+    console.log("sapphire: " + sapphire);
+
+    emerald = Math.floor((Math.random() * 12) + 1);
+    console.log("emerald: " + emerald);
+
+    diamond = Math.floor((Math.random() * 12) + 1);
+    console.log("diamond: " + diamond);
+
+    ruby = Math.floor((Math.random() * 12) + 1);
+    console.log("ruby: " + ruby);
+
+
     console.log("test");
 })
 
-var addValueToCrystal //function to be passed to each crystal
-var sapphire
-var emerald
-var diamond
-var ruby
