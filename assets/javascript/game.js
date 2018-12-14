ce615f9fae5324df635358bcc64691ac1765538c
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2018-12-14 12:52:23 -0800

    working points var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,14 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
-var addValueToCrystals 
 var computerNumber = 0
 var sapphire = 0
 var emerald = 0
 var diamond = 0
 var ruby = 0
+var points = 0
+var wins = 0
+var loses = 0
 
 var startGame = $("#startButton").on("click", function() {
     computerNumber = Math.floor((Math.random() * 120) + 19);
@@ -16,16 +18,36 @@ var startGame = $("#startButton").on("click", function() {
     sapphire = Math.floor((Math.random() * 12) + 1);
     console.log("sapphire: " + sapphire);
 
+
     emerald = Math.floor((Math.random() * 12) + 1);
     console.log("emerald: " + emerald);
+
 
     diamond = Math.floor((Math.random() * 12) + 1);
     console.log("diamond: " + diamond);
 
+
     ruby = Math.floor((Math.random() * 12) + 1);
     console.log("ruby: " + ruby);
 
-
-    console.log("test");
 })
 
+$("#sapphire").on("click", function() {
+    points = points + sapphire;
+    $(".points").text(points);
+})
+
+$("#emerald").on("click", function() {
+    points = points + emerald;
+    $(".points").text(points);
+})
+
+$("#diamond").on("click", function() {
+    points = points + diamond;
+    $(".points").text(points); 
+})
+
+$("#ruby").on("click", function() {
+    points = points + ruby;
+    $(".points").text(points);
+})
