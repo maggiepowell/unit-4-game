c665374dcb934ca90fd214a44efd36f5d63f0d56
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2018-12-14 14:28:33 -0800

    fixed points <p> zeroing issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,43 +11,57 @@ var points = 0
 var wins = 0
 var loses = 0
 
-var startGame = $("#startButton").on("click", function() {
+$("#startButton").on("click", startGame())
+
+function startGame() {
+    points = 0;
+    $(".points").text("Points Collected: " + points);
     computerNumber = Math.floor((Math.random() * 120) + 19);
     $(".computerNumber").text(computerNumber + " is your number to match!");
-
     sapphire = Math.floor((Math.random() * 12) + 1);
-    console.log("sapphire: " + sapphire);
-
-
     emerald = Math.floor((Math.random() * 12) + 1);
-    console.log("emerald: " + emerald);
-
-
     diamond = Math.floor((Math.random() * 12) + 1);
-    console.log("diamond: " + diamond);
-
-
     ruby = Math.floor((Math.random() * 12) + 1);
-    console.log("ruby: " + ruby);
-
-})
+}
 
 $("#sapphire").on("click", function() {
     points = points + sapphire;
-    $(".points").text(points);
+    $(".points").text("Points Collected: " + points);
+    checkWins();
+    console.log("points: " + points);
 })
 
 $("#emerald").on("click", function() {
     points = points + emerald;
-    $(".points").text(points);
+    $(".points").text("Points Collected: " + points);
+    checkWins();
+    console.log("points: " + points);
 })
 
 $("#diamond").on("click", function() {
     points = points + diamond;
-    $(".points").text(points); 
+    $(".points").text("Points Collected: " + points);
+    checkWins();
+    console.log("points: " + points);
 })
 
 $("#ruby").on("click", function() {
     points = points + ruby;
-    $(".points").text(points);
+    $(".points").text("Points Collected: " + points);
+    checkWins();
+    console.log("points: " + points);
 })
+
+function checkWins() {
+if (points === computerNumber) {
+    alert("You Won!");
+    wins++;
+    $('.wins').text ("Wins: " + wins);
+    startGame();
+}
+else if (points > computerNumber) {
+    alert("You Lost!");
+    loses++;
+    $('.loses').text ("Loses: " + loses);
+    startGame();
+}}
